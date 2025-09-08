16276cd4b086938c70d93c988e7357503240ea7f
Mikhail Malinin <m.malinin@melscience.com>
2025-09-08 13:29:34 +0300

    Initial project setup

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+﻿function createConfig()
+{
+    return {
+        input: 'src/index.js',
+        output: {
+            file: 'dist/bundle.js',
+        },
+    }
+}
+
+export default (commandLineArgs) => {
+    console.log(commandLineArgs, typeof commandLineArgs);
+    return createConfig();
+};
+
